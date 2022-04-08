983a086463a5d79287d71aa2768552d12b19be78
Alex Thompson <alexthompsondevelopment@gmail.com>
2022-04-08 11:53:05 +1000

    implement social media links in settings

---
+++ AussieNews/Model/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  AussieNews
+//
+//  Created by Alexander Thompson on 8/4/2022.
+//
+
+import Foundation
+
+enum SocialMedia {
+    
+    static let websiteURL           = "appNerd.com.au"
+    
+    static let linkedInUsername     = "Alexander-Thompson-847a6486"
+    static let linkedInURL          = "https://www.linkedin.com/in/\(SocialMedia.linkedInUsername)"
+    
+    static let instagramUsername    = "AlexThompsonDevelopment"
+    static let instagramURL         = "https://instagram.com/\(SocialMedia.instagramUsername)"
+    
+    static let githubUsername       = "AlexThompsonDevelopment"
+    static let githubURL            = "https://github.com/\(SocialMedia.githubUsername)"
+}
